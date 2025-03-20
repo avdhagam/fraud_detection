3a2ce01d4041cec44015fdf15ab8caa2ad5c266d
avdhagam <davanisri@gmail.com>
2025-03-20 12:24:51 +0530

    Dummy

---
+++ src/main/java/com/cars24/fraud_detection/data/entity/InsightsEntity.java
@@ -0,0 +1,16 @@
+package com.cars24.fraud_detection.data.entity;
+
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public class InsightsEntity {
+    private String doctype;
+    private String status;
+    private String documentName;
+    private Double score;
+    private String description;
+    private LocalDateTime uploadedAt;
+
+}
